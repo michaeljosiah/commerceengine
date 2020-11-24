8bdf4b5fed41449b4f32248363026e4b24487b33
Michael Josiah <mjosiah@nextgensystems.co.uk>
2020-11-24 22:36:57 +0000

    Add ProductService and its interface

---
+++ src/CommerceEngine.Application/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommerceEngine.Core.Entities;
+
+namespace CommerceEngine.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Product GetProductByName(string productName);
+    }
+}
---
+++ src/CommerceEngine.Application/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommerceEngine.Application.Interfaces;
+using CommerceEngine.Core.Entities;
+using CommerceEngine.Core.Interfaces;
+
+namespace CommerceEngine.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public Product GetProductByName(string productName)
+        {
+            return _productRepository.GetProductByName(productName);
+        }
+    }
+}
